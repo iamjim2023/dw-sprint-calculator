6ca89d5c6d1ed14a227bc93f43af7dcfab1d15d6
Jimuel Madlangsakay <jmuelkoh22@gmail.com>
2023-02-18 15:54:57 +0800

    Update

--- script.js
+++ script.js
@@ -5,18 +5,14 @@ const operator = document.querySelector('.calculator__operator');
 const secondOperend = document.querySelector('.calculator__operend--right');
 const calculatedResult = document.querySelector('.calculator__result');
 
-function calculate(n1, operator, n2) {
+function calculate(n1, operators, n2) {
   firstOperend = n1;
-  ope = operator;
+  operator = operators;
   secondOperend = n2;
   let result = 0;
 
   // TODO : make function to operate according to the n1, n2, operator.
   // ex) if input is n1 : '1', operator : '+', n2 : '2' , 3 will be returned.
-
-  if(n1 === n2){
-
-  }
   return String(result);
 }
 
@@ -30,15 +26,19 @@ buttons.addEventListener('click', function (event) {
 
   if (target.matches('button')){
     // TODO : make your code to operate calculator
-    if (action === 'number1') {
-        firstOperend.innerText = buttonContent;
-    }
-    if (action === 'operator') {
-      console.log('operator ' + buttonContent + ' button');
+    if (action === 'number') {
+      console.log('number');
     }
 
+    if (action === 'operator') {
+      console.log('');
+    }
     if (action === 'decimal') {
-      
+      alert('No decimal please!');
+      firstOperend.innerText = "0";
+      operator.innerText = "+";
+      secondOperend.innerText = "0";
+      calculatedResult.innerText = "0";
     }
     if (action === 'clear') {
       firstOperend.innerText = "0";
@@ -46,12 +46,9 @@ buttons.addEventListener('click', function (event) {
       secondOperend.innerText = "0";
       calculatedResult.innerText = "0";
     }
-
-    if (action === 'calculate') {
-      console.log('compute button');
-    }
   }
 });
+
 // ! Advanced Challenge test and Nightmare test.
 const display = document.querySelector('.calculator__display--for-advanced');
 let firstNum, operatorForAdvanced, previousKey, previousNum;
